13658079dd2e723a03c6b4f485a725312b45449f
Miroj <zorim.rai@gmail.com>
2022-06-27 16:15:08 +0545

    manipulators

---
+++ manipulators.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << setw(0) << "Name" << setw(35) << "Address" << setw(15) << "Phone no." << endl;
+    cout << setw(0) << "Miroj" << setw(35) << "Budhanilkantha-12, Kathmandu" << setw(15) << "9818030504" << endl;
+    cout << setw(0) << "Raj" << setw(35) << "Budhanilkantha-12, Kathmandu" << setw(15) << "9818030501" << endl;
+}
