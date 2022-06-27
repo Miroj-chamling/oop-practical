29c2c0333707be9bf7817d0d764a4b2cab4df875
Miroj <zorim.rai@gmail.com>
2022-06-27 16:19:28 +0545

    swap by reference

---
+++ swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(a, b);
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+}
